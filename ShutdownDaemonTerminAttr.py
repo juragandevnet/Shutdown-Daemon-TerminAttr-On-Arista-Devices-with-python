80852571ec378c18f219a33be4e288e07f543877
juragandevnet <juragandevnet@gmail.com>
2023-03-24 16:00:44 +0800

    Version 1.2

--- ShutdownDaemonTerminAttr.py
+++ ShutdownDaemonTerminAttr.py
@@ -7,17 +7,17 @@ from netmiko import ConnectHandler
 from threading import Thread
 
 def upload(device_type, username, password, ip_address):
-    iosv2_l2 = {
+    SshLogin = {
         'device_type' : device_type,
         'ip' : ip_address,
         'username': username,
         'password': password 
     }
-    net_connect = ConnectHandler(**iosv2_l2)
+    net_connect = ConnectHandler(**SshLogin)
     output_hostname = net_connect.send_command('show run | i hostname')
-    output_ios = net_connect.send_config_set(["daemon TerminAttr","shutdown"])
+    output_eos = net_connect.send_config_set(["daemon TerminAttr","shutdown"])
     print(output_hostname)
-    print(output_ios)
+    print(output_eos)
     net_connect.disconnect()
     
 ### READ FILE ###
@@ -27,10 +27,9 @@ f = open('devices.txt','r')
 username = ("Username: ")
 password = getpass.getpass()
 
-
 ### THREAD PROCESS ###
 threads=[]
-threads = [Thread(target=upload, args=('arista_eos','catadmin_2','yj%Ve6Yu~y', ip_address)) for ip_address in f.readlines()]
+threads = [Thread(target=upload, args=('arista_eos', username, password, ip_address)) for ip_address in f.readlines()]
 for thread in threads:
     thread.start()
 for thread in threads:
